e073783e6c31bebcd3095d805a253ead8ab3733a
trung chien <chienyoyo1@gmail.com>
2025-05-12 02:28:53 +0700

    datetime

---
+++ BUS/ucBieuDoDoanhThuBL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class ucBieuDoDoanhThuBL
+    {
+        private ucBieuDoDoanhThuDL uc_BieuDoDoanhThuDL;
+        public ucBieuDoDoanhThuBL()
+        {
+            uc_BieuDoDoanhThuDL = new ucBieuDoDoanhThuDL();
+        }
+        public Decimal TinhGiaNhapTrungBinhBL(int maVL)
+        {
+            SqlDataReader reader = uc_BieuDoDoanhThuDL.TinhGiaNhapTrungBinhDL(maVL);
+            if(reader.HasRows )
+            {
+                while (reader.Read())
+                {
+                    Decimal tongGiaNhap = Convert.ToDecimal(reader[0].ToString());
+                    int TongSoLuong = Convert.ToInt32(reader[1].ToString());
+                    decimal giaNhapTrungBinh = Math.Round(tongGiaNhap / TongSoLuong,2);
+                    return giaNhapTrungBinh;
+                }
+                reader.Close();
+            }
+            return 0;
+        }
+    }
+}
---
+++ DAO/ucBieuDoDoanhThuDL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+using System.Data.SqlClient;
+using System.Net;
+using System.Xml.Linq;
+using DAO;
+using System.Security.Policy;
+namespace DTO
+{
+    public class ucBieuDoDoanhThuDL:DataProvider
+    {
+        public SqlDataReader TinhGiaNhapTrungBinhDL(int maVL)
+        {
+            string sql = $@"SELECT SUM(ctpn.GiaNhap * ctpn.SoLuong) AS TongGiaNhap,SUM(ctpn.SoLuong) AS TongSoLuong
+                           FROM ChiTietPhieuNhap ctpn
+                           WHERE ctpn.MaVL = {maVL}";
+            try
+            {
+                Connect();
+                SqlDataReader reader = MyExcuteReader(sql, CommandType.Text);
+                return reader;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+
+        }
+   
+    }
+
+}
